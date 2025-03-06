9dcc34d83957eb1a71084ee57a687e8812a47a63
Nate Levison <45641361+Cheespeasa1234@users.noreply.github.com>
2025-03-06 13:59:31 +0000

    add tasks, fix tests?

---
+++ src/main_test.ts
@@ -0,0 +1,22 @@
+import { expect } from "jsr:@std/expect";
+
+/* Tests
+
+config.ts:
+1. Check the number value of a ConfigValue is correct
+2. Check the string value of a ConfigValue is correct
+3. Check the boolean value of a ConfigValue is correct
+4. Make sure the default preset is not null and doesn't error out
+5. Make sure it properly gets config values
+6. Make sure it properly downloads presets and doesn't error out
+7. Make sure it properly uploads presets and doesn't error out
+8. Make sure it properly gets and sets presets
+
+
+sheets.ts:
+1. Make sure getValues is not null and doesn't error out
+
+analyze_responses.ts:
+
+
+*/
